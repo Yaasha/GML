3612f1504f6943aa535a11d43a8f5fdaf3e7c35b
Yaasha <davijasek@gmail.com>
2016-09-20 22:18:55 +0200

    get basic data from API TODO: substitution

--- app/src/main/java/gml/waffles/gml/SuplovaniFragment.java
+++ app/src/main/java/gml/waffles/gml/SuplovaniFragment.java
@@ -3,9 +3,12 @@ package gml.waffles.gml;
 import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.Xml;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +16,21 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -27,6 +41,7 @@ public class SuplovaniFragment extends Fragment {
     private SharedPreferences pref;
     //url variable for saving current url of web
     private String url = "";
+    private String TAG = SuplovaniFragment.class.getName();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -38,110 +53,86 @@ public class SuplovaniFragment extends Fragment {
     public void onActivityCreated(final Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        //get xml data from internet (outside of main threat)
+        class FetchData extends AsyncTask<Void, Void, String> {
 
-        //get data from settings
-        /*boolean divide = PreferenceManager.getDefaultSharedPreferences(getActivity()).getBoolean("suplovaniDivide", true);
-            web = (WebView) view.findViewById(R.id.supl);
-            web.setWebViewClient(new WebViewClient(){
-                @Override
-                public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                    if(url.contains("gml.cz")) web.loadUrl(url); //prevents from opening other websites
-                        return true;
-                }
-            });
-            pref = getActivity().getPreferences(0);
+            @Override
+            protected String doInBackground(Void... params) {
+                //define connection and reader
+                HttpURLConnection connection = null;
+                //create parser to contain xml
+                XmlPullParser parser = Xml.newPullParser();
+                //define string to contain resulting data
+                String result = "";
+                //connect and get data
+                try {
+                    //API request url
+                    URL url = new URL("https://gmlbrno.edupage.org/eduapi?apikey=98C965812D0AB3258463&cmd=getbasedata");
+                    //connect to url
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.connect();
+                    //get input from connection and define buffer to create string
+                    InputStream input = connection.getInputStream();
+                    //return null if input is empty
+                    if (input == null) return null;
+                    //temporary converting inputstream to string TODO
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuffer buffer = new StringBuffer();
+                    String line;
+                    while((line = reader.readLine()) != null){
+                        buffer.append(line);
+                    }
 
-            if(GML.isConnected(getActivity().getApplicationContext())) web.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
-            else
-            {
-                web.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
-                Toast.makeText(getActivity().getApplicationContext(), "Nepodařilo se aktualizovat stránku",Toast.LENGTH_SHORT).show();
-            }
-
-        //if divide is enabled in settings
-        if(divide){
-            //load saved url
-            String savedUrl = pref.getString("savedUrl", null);
-            if(savedUrl != null) url = savedUrl;
-            //if there is no saved url load current day
-            else {
-                //date for default url
-                SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
-                Calendar calendar = Calendar.getInstance();
-                //condition for weekend > monday
-                calendar.setFirstDayOfWeek(Calendar.SATURDAY);
-                if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY || calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) {
-                    calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-                }
-                //load default url
-                url = "http://www.gml.cz/zastupce/nove/subst_students" + date.format(calendar.getTime()) + ".htm";
-            }
-            web.loadUrl(url);
-        }else web.loadUrl("http://www.gml.cz/zastupce/nove/"); //else load whole page
-
-
-        //popup button
-        final Button button = (Button) view.findViewById(R.id.popupSupl);
-
-        //if enabled in settings
-        if(divide) {
-            //set button text
-            button.setText(urlToDate(url));
-            //popup
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    //popup webview
-                    WebView webView = new WebView(getActivity());
-                    //check for internet connection and set cache mode accordingly
-                    if (GML.isConnected(getActivity().getApplicationContext()))
-                        webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
-                    else webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
-                    //load url
-                    webView.loadUrl("http://www.gml.cz/zastupce/nove/subst_left.htm");
-                    //popup builder
-                    final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity()).setView(webView);
-                    //popup dialog
-                    final AlertDialog dialog = builder.create();
-                    //show popup
-                    dialog.show();
-                    //get screen density for window size
-                    DisplayMetrics displayMetrics = new DisplayMetrics();
-                    getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-                    float density = displayMetrics.density;
-                    //multiply density with days panel size according to http://www.gml.cz/zastupce/nove/
-                    int width = Math.round(density * 150);
-                    //set popup window size
-                    dialog.getWindow().setLayout(width, view.getHeight());
-
-                    webView.setWebViewClient(new WebViewClient() {
-                        @Override
-                        public boolean shouldOverrideUrlLoading(WebView view, String newUrl) {
-                            url = newUrl;
-                            web.loadUrl(url);
-                            dialog.dismiss();
-                            SharedPreferences.Editor editor = pref.edit();
-                            editor.putString("savedUrl", url);
-                            editor.apply();
-
-                            button.setText(urlToDate(url));
-                            return true;
+                    //set input as parse input
+                    parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+                    parser.setInput(new StringReader(buffer.toString()));
+                    //disconnect
+                    connection.disconnect();
+                    //fetch data from raw xml
+                    int state = parser.getEventType();
+                    while(state != XmlPullParser.END_DOCUMENT){
+                        switch(state){
+                            case XmlPullParser.START_TAG:
+                                //writes list of tags with their values (except for first)
+                                    result += parser.getName() + ": ";
+                                    for(int a = 1; a < parser.getAttributeCount(); a++){
+                                        result += parser.getAttributeValue(a) + ";";
+                                    }
+                                break;
+                            case XmlPullParser.END_TAG:
+                                //creates next line when tag ends
+                                result += "\n";
+                                break;
+                            default:
+                                break;
                         }
-                    });
+                        state = parser.next();
+                    }
+
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                    Log.e(TAG, "ERROR");
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                    Log.e(TAG, "ERROR");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e(TAG, "ERROR");
+                } catch (XmlPullParserException e) {
+                    e.printStackTrace();
                 }
-            });
-        }else button.setVisibility(View.INVISIBLE);*/
+                return result;
+            }
 
-
-
+            @Override
+            protected void onPostExecute(String s) {
+                //write string data to edittext view
+                EditText txt = (EditText) view.findViewById(R.id.txt);
+                txt.setText(s);
+            }
         }
-    //convert suplementation url to date
-    private String urlToDate(String url){
-        //just in case the url wont be loaded
-        if(url != null){
-        String[] day = url.split("-");
-        String t = day[2].replace("htm", "") + day[1] + ".";
-        return t;
-        }else return "Vybrat den";
+        //executes the FetchData class
+        (new FetchData()).execute();
     }
 }
